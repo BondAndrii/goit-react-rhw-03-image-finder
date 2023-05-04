525ca4287e372a90fd6c9581625ba14b8662a97e
BondAndrii <104005997+BondAndrii@users.noreply.github.com>
2023-05-04 15:20:08 +0300

    add style to Gallery and GalleryItem

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -2,10 +2,12 @@ import React from "react"
 
 import { ImageGalleryItem } from "components/ImageGalleryItem/ImageGalleryItem"
 
+import style from "./ImageGallery.module.css"
+
 export const ImageGallery = ({pictures}) => {
   console.log("in imagegallery", pictures)
   return (
-    <ul className="gallery">
+    <ul className={style.ImageGallery}>
       {pictures.map(picture => <ImageGalleryItem key={ picture.id} picture={picture} /> )}
     </ul>
   )
--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,11 +1,13 @@
 import React from "react"
 
+import style from "./ImageGalleryItem.module.css"
+
 export const ImageGalleryItem = ({picture}) => {
   const { id, previewURL, tags } = picture;
   return (
-          <li key={id} className="gallery-item">
-            <img src={previewURL} alt={tags} />
-          </li>
-        )
+    <li key={id} className={style.ImageGalleryItem }>
+            <img className={style.ImageGalleryItemImage} src={previewURL} alt={tags} />
+    </li>
+  )
 }
 
